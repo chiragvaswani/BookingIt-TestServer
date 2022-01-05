575431d022e9cbabba516237dc3ba98196c1b91e
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-06 00:48:19 +0530

    Define initialiser method that starts the server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http')
+const http = require("http");
 
-const {mongoConnect} = require('booking-it');
+const { mongoConnect } = require("booking-it");
 
-const app = require('app')
+const app = require("./app");
 
 const server = http.createServer(app);
 
 async function startServer() {
-	await mongoConnect("mongodb+srv://root:Gu9LK4MPwc8DkyH@cluster0.zonle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-	server.listen(3000, () => console.log("Server runnning on port 3000"));
+  await mongoConnect("");
+  server.listen(3000, () => console.log("Server runnning on port 3000"));
 }
 
 startServer();
