260a63cd5a3d3a32f0f9d5c2d66321defd2ef279
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-05 23:19:43 +0530

    Define function to connect to database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
+const {mongoConnect} = require('booking-it');
+
 const app = require('app')
 
 const server = http.createServer(app);
 
-server.listen(3000, () => console.log("Server runnning on port 3000"));
+async function startServer() {
+	await mongoConnect("mongodb+srv://root:Gu9LK4MPwc8DkyH@cluster0.zonle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+	server.listen(3000, () => console.log("Server runnning on port 3000"));
+}
+
+startServer();
