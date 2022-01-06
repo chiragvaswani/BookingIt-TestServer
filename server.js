cf5210ad597de3224120df27e691c552d681b61a
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-06 14:29:03 +0530

    Add comment

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = require("./app");
 const server = http.createServer(app);
 
 async function startServer() {
+  // Please pass the URL to the MongoDB database to the function given below before running the application
   await mongoConnect("");
   server.listen(3000, () => console.log("Server runnning on port 3000"));
 }
